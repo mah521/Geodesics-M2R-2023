a95bac910de69221ba24140cc46df7c1bc4d0991
mah521 <mah521@ic.ac.uk>
2023-06-06 12:47:55 +0100

    modified:   stages/stage1.py Attempting to plot 2D line.

--- stages/stage1.py
+++ stages/stage1.py
@@ -1,9 +1,22 @@
 # Stage 1: Stage 1 - Visualisation of piecewise linear curves on the plane and
 # torus.
 
-gamma = []  # List of N points in 2D here:
+import plotly.graph_objects as go
+import numpy as np
 
+
+gamma = []  # List of N points in 2D here:
+N = len(gamma)
 
 # Plot them on the plane
 
 x_coords, y_coords = zip(*gamma)
+
+line = go.scatter3d(
+    x=x_coords,
+    y=y_coords,
+    z=np.zeroes(N),
+    mode="lines",)
+
+Line_fig = go.Figure(data=[line])
+Line_fig.show()
